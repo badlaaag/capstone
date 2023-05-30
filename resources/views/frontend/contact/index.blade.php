4aa86b6d0135f69d291c677a70ef4ec8fd3ff0b1
vins <106815864+badlaaag@users.noreply.github.com>
2023-05-30 23:52:57 +0800

    Update contact css
    Update contact css

--- resources/views/frontend/contact/index.blade.php
+++ resources/views/frontend/contact/index.blade.php
@@ -16,7 +16,7 @@
         </nav>
     </div>
 </div>
-<div class="contact-area pt-3 pb-5">
+<div class="contact-area pt-5 pb-5">
 	<div class="container">
 		<div class="contact-map mb-10 pb-5">
 			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3858.6877769158345!2d121.13563731543258!3d14.73023597783421!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397bb18144bf653%3A0x34e13592dd4050d0!2sRodriguez%20Hwy%2C%20Rodriguez%2C%20Rizal!5e0!3m2!1sen!2sph!4v1679020338140!5m2!1sen!2sph" width="600" height="450" class="border-0 w-100" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
